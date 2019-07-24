bdb34a6bfd807f39808ab853767fdc86dfb7ba59
Joakim Hove <joakim.hove@gmail.com>
2019-07-24 12:02:05 +0200

    Attempt to use clang-format

--- user/c++.el
+++ user/c++.el
@@ -1,3 +1,8 @@
+(defun has-clang-format()
+  "Reformat buffer if .clang-format exists in the projectile root."
+  (f-exists? (expand-file-name ".clang-format" (projectile-project-root))))
+
+
 (defun jh-compile ()
   (interactive)
   (require 'helm-make)
@@ -7,7 +12,18 @@
 (defun my-c++-mode-hook ()
   (setq c-basic-offset 4)
   (c-set-offset 'innamespace 0)
-  (c-set-offset 'substatement-open 0))
+  (c-set-offset 'substatement-open 0)
+
+  (when (has-clang-format)
+    (progn
+      (define-key evil-normal-state-map "=" 'clang-format-region))))
+  ;;(define-key c++-mode-map "=" 'clang-format-region))))
 
 
-(add-hook 'c++-mode-hook 'my-c++-mode-hook)
+(defun init-c++()
+  (interactive)
+  (my-c++-mode-hook))
+
+(add-hook 'c++-mode-hook 'my-c++-mode-hook 't)
+
+
